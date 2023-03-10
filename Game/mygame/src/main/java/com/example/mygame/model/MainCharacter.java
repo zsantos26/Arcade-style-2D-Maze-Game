f8d0d1120a5cfb5d14e3d0a3bcb6f699a4057027
thedarianwong <thedarianwong@gmail.com>
2023-03-09 21:42:33 -0800

    Added MainChar, Cells, GameView Classes

--- Game/mygame/src/main/java/com/example/mygame/model/MainCharacter.java
+++ Game/mygame/src/main/java/com/example/mygame/model/MainCharacter.java
@@ -1,5 +1,58 @@
 package com.example.mygame.model;
 
+import java.awt.image.BufferedImage;
 public class MainCharacter {
-  
+    private int x; // x-coordinate of the main character
+    private int y; // y-coordinate of the main character
+    private int width; // width of the main character
+    private int height; // height of the main character
+    private BufferedImage image; // image of the main character
+
+    public MainCharacter(int x, int y, int width, int height, BufferedImage image) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
 }
--- Game/mygame/src/main/java/com/example/mygame/view/GameView.java
+++ Game/mygame/src/main/java/com/example/mygame/view/GameView.java
@@ -1,5 +1,47 @@
 package com.example.mygame.view;
 
-public class GameView {
-  
+import javax.swing.*;
+import java.awt.*;
+
+public class GameView extends JFrame {
+    
+    private JPanel gamePanel;
+    private JButton startButton;
+    private JButton exitButton;
+    private JLabel scoreLabel;
+    
+    public GameView() {
+        // Set up the main window
+        setTitle("My Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        
+        // Create the game panel
+        gamePanel = new JPanel();
+        gamePanel.setBackground(Color.BLACK);
+        
+        // Create the start and exit buttons
+        startButton = new JButton("Start Game");
+        exitButton = new JButton("Exit Game");
+        
+        // Create the score label
+        scoreLabel = new JLabel("Score: 0");
+        scoreLabel.setForeground(Color.WHITE);
+        
+        // Add the components to the main window
+        add(gamePanel, BorderLayout.CENTER);
+        add(startButton, BorderLayout.NORTH);
+        add(exitButton, BorderLayout.SOUTH);
+        add(scoreLabel, BorderLayout.EAST);
+        
+        // Show the main window
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        // Create a new GameWorld object
+        new GameView();
+    }
 }
