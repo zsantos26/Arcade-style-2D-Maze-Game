beedd412b78d1f4310d82b3236ce25c9449297d9
thedarianwong <thedarianwong@gmail.com>
2023-03-14 11:16:40 -0700

    Modified StaticRewards

--- Game/mygame/src/main/java/com/example/characters/StaticRewards.java
+++ Game/mygame/src/main/java/com/example/characters/StaticRewards.java
@@ -1,10 +1,97 @@
 package com.example.characters;
 
+import java.io.IOException;
+import java.util.Random;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
-public class StaticRewards extends Rewards {
-    public StaticRewards(int rewardAmount, int x, int y) {
-        super(rewardAmount, x, y);
+import java.awt.Graphics2D;
+import com.example.game.CollisionDetector;
+import com.example.game.GameEngine;
+
+
+public class StaticRewards extends Character {
+    private int duration;
+    private int randomTimeBetweenBonus;  // Number of ticks before next bonus appears
+    private int rewardAmount;
+    private Random random;
+    private CollisionDetector collisionDetector;
+    private GameEngine gameBarrier;
+    private boolean visible;
+    public StaticRewards(int rewardAmount, int x, int y, GameEngine gameEngine, CollisionDetector collisionDetector) {
+        super(x, y,"");
+        this.visible = true;
+        this.randomTimeBetweenBonus = 0;
+        this.random = new Random();
+        this.collisionDetector = collisionDetector;
+        this.gameBarrier = gameEngine;
+        getStaticRewardsSprite();
     }
+
+    // getters and setters for the visible field
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public int getRewardAmount() {
+        return rewardAmount;
+    }
+
+    public void setRewardAmount(int amount) {
+        rewardAmount = amount;
+    }
+
+    public boolean onReward(int charX, int charY) {
+        return x == charX && y == charY;
+    }
+
+    public int claimReward() {
+        int scoreEarned = getRewardAmount();
+        setRewardAmount(0);
+        return scoreEarned;
+    }
+
+    public void update() {
+        visible = true;
+                duration  = random.nextInt(20); // Between 200 - 400
+                setRewardAmount(100);  // Set new bonus reward amount to 100;
+
+                do{
+                    x = random.nextInt(20);
+                    y = random.nextInt(20);
+                    x = x * gameBarrier.cellSize;
+                    y = y * gameBarrier.cellSize;
+                    System.out.println("X: " + x + " Y: " + y);
+                    System.out.println("IT RELOCATE");
+                }
+                while (collisionDetector.checkCells(this)==true);
+    }
+
+
+
+    public void getBonusRewardsSprite() {
+        try{
+            bonusReward = ImageIO.read(getClass().getResourceAsStream("/images/book/specialRewards.png"));
+
+        }catch(IOException e){
+            System.out.println("FAIL FIL FAIL");
+            e.printStackTrace();
+            System.out.println("Error loading images: " + e.getMessage());
+        }
+    }
+
+    public void draw(Graphics2D graphics){
+        if(isVisible()){
+            BufferedImage image = bonusReward;
+            graphics.drawImage(image, x, y, gameBarrier.cellSize, gameBarrier.cellSize, null);
+        }
+    }
+}
+
 
 }
 
