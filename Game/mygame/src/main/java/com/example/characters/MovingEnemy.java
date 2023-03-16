001ac66c536c841ae56516c3cd7528320cd324a8
thedarianwong <thedarianwong@gmail.com>
2023-03-15 21:57:38 -0700

    added moveTowards(mainchar) not tested

--- Game/mygame/src/main/java/com/example/characters/MovingEnemy.java
+++ Game/mygame/src/main/java/com/example/characters/MovingEnemy.java
@@ -14,9 +14,34 @@ public class MovingEnemy extends Character {
         this.gameBarrier = gameEngine;  // This is the gameBarrier object
     }
 
-    public void moveTowards(Character character) {
-        // implementation to move towards the main character
+public void moveTowards(MainCharacter mainChar) {
+    int mainCharX = mainChar.getX() + gameBarrier.cellSize / 2;
+    int mainCharY = mainChar.getY() + gameBarrier.cellSize / 2;
+    int enemyX = x + gameBarrier.cellSize / 2;
+    int enemyY = y + gameBarrier.cellSize / 2;
+
+    // Calculate the distance between the enemy and the main character
+    double distance = Math.sqrt(Math.pow(mainCharX - enemyX, 2) + Math.pow(mainCharY - enemyY, 2));
+
+    // If the distance is less than or equal to the speed, move the enemy to the main character's cell
+    if (distance <= gameBarrier .cellSize) {
+        x = mainCharX - gameBarrier.cellSize / 2;
+        y = mainCharY - gameBarrier.cellSize / 2;
+        return;
     }
+
+    // Calculate the x and y components of the direction vector from the enemy to the main character
+    double dx = (mainCharX - enemyX) / distance;
+    double dy = (mainCharY - enemyY) / distance;
+
+    // Move the enemy in the direction of the main character by the speed
+    x += (int) (dx * gameBarrier.cellSize);
+    y += (int) (dy * gameBarrier.cellSize);
+
+    // Round the enemy's position to the nearest cell
+    x = (x / gameBarrier.cellSize) * gameBarrier.cellSize;
+    y = (y / gameBarrier.cellSize) * gameBarrier.cellSize;
+}
 
     /*
     This method is called when the character is created to load the character's sprite
