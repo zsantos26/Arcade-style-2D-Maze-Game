afe69bce3b211194c57831358428e4310b711b44
thedarianwong <thedarianwong@gmail.com>
2023-03-15 21:39:21 -0700

    MovingEnemy extends Character

--- Game/mygame/src/main/java/com/example/characters/MovingEnemy.java
+++ Game/mygame/src/main/java/com/example/characters/MovingEnemy.java
@@ -1,9 +1,9 @@
 package com.example.characters;
 
 
-public class MovingEnemy extends Enemy {
+public class MovingEnemy extends Character {
     public MovingEnemy(int x, int y, int damage) {
-        super(x, y, damage);
+        super(x, y, "down");
     }
 
     public void moveTowards(Character character) {
