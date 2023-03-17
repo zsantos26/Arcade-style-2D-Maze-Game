a9d831daa275442429edf2e1d17cc0d6aa782265
thedarianwong <thedarianwong@gmail.com>
2023-03-16 20:26:18 -0700

    added collision to static enemy

--- Game/mygame/src/main/java/com/example/characters/StaticEnemy.java
+++ Game/mygame/src/main/java/com/example/characters/StaticEnemy.java
@@ -6,6 +6,8 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
+
+import com.example.Main;
 import com.example.game.GameEngine;
 
 public class StaticEnemy extends Character {
@@ -13,10 +15,13 @@ public class StaticEnemy extends Character {
     private GameEngine gameBarrier;
     private Random random;
     private boolean visible;
+    private boolean isDetected;
+
     public StaticEnemy(int x, int y, int damage, GameEngine gameEngine) {
         super(x, y, "");
         this.random = new Random();
         this.gameBarrier = gameEngine;
+        this.isDetected = false;
         getStaticEnemySprite();
     }
 
@@ -37,10 +42,36 @@ public class StaticEnemy extends Character {
         damageAmount = amount;
     }
 
-    public void update() {
-        //Still thinking if I should make the Static Rewards Fixed or Randomly Spawned?????
+    public void update(MainCharacter mainChar) {
+        // Still thinking if I should make the Static Rewards Fixed or Randomly
+        // Spawned?????
+        int amount = random.nextInt(200);
+        setDamageAmount(amount);
+        punishment(mainChar);
+    }
+
+    public void punishment(MainCharacter mainChar) {
+        int scoreEarned = getDamageAmount();
+        int dx = mainChar.getX() - x;
+        int dy = mainChar.getY() - y;
+        double distance = Math.sqrt(dx * dx + dy * dy);
+
+        // check if enemy is already adjacent to the main character
+        if (distance < gameBarrier.cellSize) {
+            mainChar.score -= scoreEarned;
+            System.out.println("Score: " + mainChar.score);
+            isDetected = true;
+            spawning();
+        }
+    }
+
+    public void spawning() {
         visible = true;
-        if(gameBarrier.collisionDetector.checkCells(this)==true){
+        x = random.nextInt(20);
+        y = random.nextInt(20);
+        x = x * gameBarrier.cellSize;
+        y = y * gameBarrier.cellSize;
+        while (gameBarrier.collisionDetector.checkCells(this) == true) {
             x = random.nextInt(20);
             y = random.nextInt(20);
             x = x * gameBarrier.cellSize;
@@ -49,28 +80,27 @@ public class StaticEnemy extends Character {
             System.out.println("IT RELOCATE FOR STATIC");
         }
         int amount = random.nextInt(200);
-        setDamageAmount(amount);  // Set new bonus reward amount to 100;
+        setDamageAmount(amount); // Set new bonus reward amount to 100;
     }
 
     public void getStaticEnemySprite() {
-        try{
+        try {
             staticEnemy1 = ImageIO.read(getClass().getResourceAsStream("/images/cctv/cctv_left-1.png.png"));
             staticEnemy2 = ImageIO.read(getClass().getResourceAsStream("/images/cctv/cctv-1.png.png"));
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("FAIL FIL FAIL");
             e.printStackTrace();
             System.out.println("Error loading images: " + e.getMessage());
         }
     }
 
-    public void draw(Graphics2D graphics){
-        if(isVisible()){
+    public void draw(Graphics2D graphics) {
+        if (isVisible()) {
             BufferedImage image = null;
             int spriteGenerate = random.nextInt(1);
-            if(spriteGenerate == 0){
+            if (spriteGenerate == 0) {
                 image = staticEnemy1;
-            }
-            else{
+            } else {
                 image = staticEnemy2;
             }
             graphics.drawImage(image, x, y, gameBarrier.cellSize, gameBarrier.cellSize, null);
