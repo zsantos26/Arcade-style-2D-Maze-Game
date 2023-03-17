5560b892003bc413744a377ac5201bb3410c2f23
zsantos <zsantos@sfu.ca>
2023-03-17 12:07:24 +0000

    Update MainMenu.java - Changed for Play screen (instead of difficulty), and modified comments

--- Game/mygame/src/main/java/com/example/menu/MainMenu.java
+++ Game/mygame/src/main/java/com/example/menu/MainMenu.java
@@ -1,4 +1,4 @@
-package com.example.menu;
+package groupid;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -9,71 +9,71 @@ import javax.swing.plaf.metal.MetalButtonUI;
 
 public class MainMenu extends JFrame implements ActionListener {
 
-    // buttons
+    // Declare 3 JButton to represent 3 buttons to appear on screen
     JButton playButton, howToPlayButton, exitButton;
 
-    // constructor
+    // Constructor
     public MainMenu() {
-        // set the title
+        // Set title
         setTitle("SFU Escape");
 
-        // set the background color of the JFrame
+        // Set the background colour of the JFrame to SFU Primary colours
         getContentPane().setBackground(new Color(204, 6, 51));
 
-        // create the buttons
+        // Initialize buttons with representative text
         playButton = new JButton("Play");
         howToPlayButton = new JButton("How to Play");
         exitButton = new JButton("Exit");
 
-        // set the preferred size of the buttons
+        // Set the colour and UI of the buttons, aswell as the preferred size of the buttons
         playButton.setUI(new MetalButtonUI());
         playButton.setPreferredSize(new Dimension(100, 50));
-        playButton.setBackground(new Color(166, 25, 46));
+        playButton.setBackground(new Color(166, 25, 46));  // Set the background colour to SFU secondary colour
         playButton.setForeground(Color.WHITE);
+        playButton.setFocusPainted(false);  // When a button is selected (but not clicked), disables visual cue
 
         howToPlayButton.setUI(new MetalButtonUI());
         howToPlayButton.setPreferredSize(new Dimension(130, 50));
-        howToPlayButton.setBackground(new Color(166, 25, 46));
+        howToPlayButton.setBackground(new Color(166, 25, 46));  // Set the background colour to SFU secondary colour
         howToPlayButton.setForeground(Color.WHITE);
+        howToPlayButton.setFocusPainted(false);  // When a button is selected (but not clicked), disables visual cue
 
         exitButton.setUI(new MetalButtonUI());
-        exitButton.setPreferredSize(new Dimension(100, 50));
+        exitButton.setPreferredSize(new Dimension(100, 50));  // Set the background colour to SFU secondary colour
         exitButton.setBackground(new Color(166, 25, 46));
         exitButton.setForeground(Color.WHITE);
+        exitButton.setFocusPainted(false);  // When a button is selected (but not clicked), disables visual cue
 
-        playButton.setFocusPainted(false);
-        howToPlayButton.setFocusPainted(false);
-        exitButton.setFocusPainted(false);
-
-        // add action listeners to the buttons
+        // Add action listeners. Allows for program to respond to button clicks.
         playButton.addActionListener(this);
         howToPlayButton.addActionListener(this);
         exitButton.addActionListener(this);
 
-        // create the panel for the buttons
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 65, 20)); // set the layout manager to FlowLayout and center the buttons
-        buttonPanel.setBackground(new Color(204, 6, 51));
+        // Create the buttonPanel
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 65, 20)); // Set the layout manager to FlowLayout and center the buttons
+        buttonPanel.setBackground(new Color(204, 6, 51));  // Set to match background colours
         JLabel header = new JLabel("SFU Break");
         header.setFont(new Font("Arial", Font.BOLD, 50));
         header.setForeground(Color.WHITE);
-        header.setAlignmentX(Component.CENTER_ALIGNMENT); // center the header horizontally
-        header.setHorizontalAlignment(SwingConstants.CENTER); // center the header text
+        header.setAlignmentX(Component.CENTER_ALIGNMENT); // Center the header horizontally
+        header.setHorizontalAlignment(SwingConstants.CENTER); // Center the header text
+        // Add buttons to panel
         buttonPanel.add(playButton);
         buttonPanel.add(howToPlayButton);
         buttonPanel.add(exitButton);
 
-        // add the panel to the frame
+        // Add the panel to the frame
         Box container = Box.createVerticalBox(); // create a vertical box container
-        container.setBorder(new EmptyBorder(75, 50, 0, 50)); // add a top margin to the container
+        container.setBorder(new EmptyBorder(75, 50, 0, 50)); // Add top, left and right margins to the container
         container.setBackground(new Color(204, 6, 51));
-        container.add(header);
-        container.add(Box.createVerticalStrut(110)); // add a vertical gap between the header and the buttons
+        container.add(header);  // Add header
+        container.add(Box.createVerticalStrut(110)); // Add vertical gap between the header and the buttons
         container.add(buttonPanel);
         add(container);
 
-        // set the size and center the frame
+        // Set the initial. and minimum, size of the frame
         setSize(1280, 720);
-        setMinimumSize(new Dimension(640, 360)); // set the minimum size of the window
+        setMinimumSize(new Dimension(640, 360)); 
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -82,8 +82,8 @@ public class MainMenu extends JFrame implements ActionListener {
     // button actions
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == playButton) {
-            ChooseDifficulty chooseDifficulty = new ChooseDifficulty();
-            chooseDifficulty.setVisible(true);
+            PlayScreen playScreen = new PlayScreen();
+            playScreen.setVisible(true);
             dispose();
         } else if (e.getSource() == howToPlayButton) {
             HowToPlay howToPlay = new HowToPlay();
@@ -93,7 +93,5 @@ public class MainMenu extends JFrame implements ActionListener {
             System.exit(0);
         }
     }
+
 }
-
-
-
