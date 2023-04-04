211e34d62c90909976d25899af462e39fbff26ac
Kai Cho <sca268@sfu.ca>
2023-04-04 16:58:06 -0700

    BonusRewardsTest

--- Game/mygame/src/test/java/BonusRewardsTest.java
+++ Game/mygame/src/test/java/BonusRewardsTest.java
@@ -2,12 +2,14 @@ import com.example.abstractfactory.AbstractFactory;
 import com.example.abstractfactory.GameObjectFactory;
 import com.example.characters.MainCharacter;
 import com.example.characters.BonusRewards;
+import com.example.characters.MovingEnemy;
 import com.example.characters.StaticRewards;
 import com.example.game.GameEngine;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import com.example.characters.MovingEnemy;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,8 +28,35 @@ public class BonusRewardsTest {
         GameEngine gameEngine = new GameEngine(gameObjectFactory);
         gameEngine.startGameThread();
         this.mainChar = gameEngine.mainChar;
-        this.bonusRewards = gameEngine.bonusRewards;
+        bonusRewards = new BonusRewards(50, 50, 30, 30, gameEngine);
     }
+
+    @Test
+    public void testSetRewardAmount() {
+        bonusRewards.setRewardAmount(200);
+        Assert.assertEquals(200, this.bonusRewards.getRewardAmount());
+    }
+
+    @Test
+    public void testSpawning() {
+        bonusRewards.spawning();
+        assertFalse(bonusRewards.checkCollision()); // should not collide with any barriers
+    }
+
+    @Test
+    public void testGetBonusRewardsSprite() {
+        assertNotNull(bonusRewards.bonusReward);
+
+    }
+    @Test
+    public void testCheckCollision() {
+        bonusRewards.setX(50); // move rewards to (50, 0)
+        bonusRewards.setY(50); // move rewards to (50, 50)
+        mainChar.setX(50); // move main character to (50, 0)
+        mainChar.setY(50); // move main character to (50, 50)
+        assertTrue(bonusRewards.checkCollision()); // should collide with the main character
+    }
+
 
 
 }
