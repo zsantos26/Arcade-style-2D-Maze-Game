6c3e768d8f75b55525006f9a6ce7d02280bb6072
Kai Cho <sca268@sfu.ca>
2023-04-04 17:12:12 -0700

    BonusRewardsTest

--- Game/mygame/src/test/java/BonusRewardsTest.java
+++ Game/mygame/src/test/java/BonusRewardsTest.java
@@ -2,14 +2,11 @@ import com.example.abstractfactory.AbstractFactory;
 import com.example.abstractfactory.GameObjectFactory;
 import com.example.characters.MainCharacter;
 import com.example.characters.BonusRewards;
-import com.example.characters.MovingEnemy;
-import com.example.characters.StaticRewards;
 import com.example.game.GameEngine;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import com.example.characters.MovingEnemy;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -56,6 +53,60 @@ public class BonusRewardsTest {
         mainChar.setY(50); // move main character to (50, 50)
         assertTrue(bonusRewards.checkCollision()); // should collide with the main character
     }
+
+
+
+    @Test
+    public void testClaimReward() {
+
+        bonusRewards.setX(50); // move rewards to (50, 0)
+        bonusRewards.setY(50); // move rewards to (50, 50)
+        mainChar.setX(50); // move main character to (50, 0)
+        mainChar.setY(50); // move main character to (50, 50)
+        mainChar.score = 0;
+        Assert.assertEquals(0, mainChar.score);
+        bonusRewards.setRewardAmount(200);
+        bonusRewards.update(mainChar);
+        assertTrue(mainChar.score == 200);
+    }
+
+    @Test
+    public void reAllocateClaimReward() {
+
+        bonusRewards.setX(50); // move rewards to (50, 0)
+        bonusRewards.setY(50); // move rewards to (50, 50)
+        mainChar.setX(50); // move main character to (50, 0)
+        mainChar.setY(50); // move main character to (50, 50)
+        mainChar.score = 0;
+        Assert.assertEquals(0, mainChar.score);
+        bonusRewards.setRewardAmount(200);
+        bonusRewards.update(mainChar);
+        bonusRewards.spawning();
+        assertTrue(mainChar.score == 200);
+    }
+
+    @Test
+    public void testDraw() {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/images/book/specialRewards.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Graphics2D graphics = image.createGraphics();
+        this.bonusRewards.draw(graphics);
+        Assert.assertNotNull(image);
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
